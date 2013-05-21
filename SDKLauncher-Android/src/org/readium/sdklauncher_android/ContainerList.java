92e2c930fc64007add19a20976a4feda3b203a9a
hqwu <afd.hqwu@gmail.com>
2013-05-21 09:58:50 +0800

    add get inner books method

--- SDKLauncher-Android/src/org/readium/sdklauncher_android/ContainerList.java
+++ SDKLauncher-Android/src/org/readium/sdklauncher_android/ContainerList.java
@@ -3,12 +3,15 @@
  */
 package org.readium.sdklauncher_android;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -26,12 +29,10 @@ public class ContainerList extends Activity {
         setContentView(R.layout.container_list);
         context = this;
         final ListView view = (ListView)findViewById(R.id.containerList);
-        String[] values = new String[]{"Android", "iPhone", "iPod"};
+        //String[] values = new String[]{"Android", "iPhone", "iPod"};
         
-        final ArrayList<String> list = new ArrayList<String>();
-        for(int i = 0; i < values.length;i++){
-            list.add(values[i]);
-        }
+        final ArrayList<String> list = getInnerBooks();
+        
         BookListAdapter bookListAdapter = new BookListAdapter(this,list);
         view.setAdapter(bookListAdapter);
         view.setOnItemClickListener(new ListView.OnItemClickListener(){
@@ -50,5 +51,25 @@ public class ContainerList extends Activity {
 			}  	
         });
     }
+    private ArrayList<String> getInnerBooks() {
+    	ArrayList<String> list = new ArrayList<String>();
+		AssetManager assetManager = context.getAssets();
+		try {
+			String[] files = assetManager.list("TestData");
+			for (int i = 0; i < files.length; i++) {
+				String temp = files[i];
+				if (temp.length() > 5
+						&& temp.substring(temp.length() - 5).equals(".epub")) {
+
+					list.add(files[i]);
+					Log.e("the books", files[i]);
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
 
 }
