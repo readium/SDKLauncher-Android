048707fc98fde0117acbb3084489cf2904688a1c
zwhu <afd.zwhu@gmail.com>
2013-07-29 20:37:09 +0800

    add download from url method

---
+++ Readium_SDK_Test_Android/src/com/anfengde/epub/DownloadFile.java
@@ -0,0 +1,71 @@
+package com.anfengde.epub;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import android.os.Environment;
+
+public class DownloadFile {
+    public static void downLoad(String dLName) {
+        String path = "epubtest";
+        String fileName = dLName;
+        OutputStream output = null;
+        boolean bookExist = false;
+        try {
+            String tempDL = URLEncoder.encode(dLName, "UTF-8").replace("+",
+                    "%20");
+            String temp = "http://192.168.66.254/incoming/epub/test/data/"
+                    + tempDL;
+            URL url = new URL(temp);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            String SDCard = Environment.getExternalStorageDirectory() + "";
+            String pathName = SDCard + "/" + path + "/" + fileName;
+
+            File file = new File(pathName);
+            if (fileName == "bookName.ini" && file.exists())
+                file.delete();
+            if (fileName == "expectedData.ini" && file.exists())
+                file.delete();
+            if (fileName == "TestData.ini" && file.exists())
+                file.delete();
+            if (file.exists()) {
+                System.out.println("exits");
+                bookExist = true;
+            } else {
+                InputStream input = conn.getInputStream();
+                String dir = SDCard + "/" + path;
+                new File(dir).mkdirs();
+                file.createNewFile();
+                output = new FileOutputStream(file);
+                byte[] buffer = new byte[4 * 1024];
+                int len;
+                while ((len = input.read(buffer)) != -1) {
+                    output.write(buffer, 0, len);
+                }
+                output.flush();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (!bookExist) {
+                try {
+                    output.close();
+                    System.out.println("download success");
+                } catch (IOException e) {
+                    System.out.println("download fail");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- Readium_SDK_Test_Android/src/com/anfengde/epub/test/xmlReadTest.java
+++ Readium_SDK_Test_Android/src/com/anfengde/epub/test/xmlReadTest.java
@@ -4,6 +4,7 @@ import junit.framework.Assert;
 import android.os.Environment;
 import android.test.AndroidTestCase;
 
+import com.anfengde.epub.DownloadFile;
 import com.anfengde.epub.XmlReader;
 
 public class xmlReadTest extends AndroidTestCase {
@@ -13,6 +14,16 @@ public class xmlReadTest extends AndroidTestCase {
 	public void testReadXml() throws Exception {
 		XmlReader read = new XmlReader();
 		Assert.assertEquals("Creative Commons - A Shared Culture.epub", read
-				.getValue(testPath + "bookName.xml", "epubBook/books/bookname").get(0));
+				.getValue(testPath + "bookName.xml", "epubBook/books/bookname")
+				.get(0));
+	}
+
+	public void testLoadFiles() throws Exception {
+		XmlReader read = new XmlReader();
+		//DownloadFile down = new DownloadFile();
+		String bookname = read
+				.getValue(testPath + "bookName.xml", "epubBook/books/bookname")
+				.get(0);
+		DownloadFile.downLoad(bookname);
 	}
 }
