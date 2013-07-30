5997c141ceccb5c472893609d9b5f2b16b3a7528
zwhu <afd.zwhu@gmail.com>
2013-07-30 14:07:03 +0800

    add read xml method and download method

--- Readium_SDK_Test_Android/src/com/anfengde/epub/test/xmlReadTest.java
+++ Readium_SDK_Test_Android/src/com/anfengde/epub/test/xmlReadTest.java
@@ -1,16 +1,28 @@
 package com.anfengde.epub.test;
 
+import java.util.List;
+
 import junit.framework.Assert;
+import android.os.Environment;
 import android.test.AndroidTestCase;
 
+import com.anfengde.epub.DownloadFile;
 import com.anfengde.epub.XmlReader;
 
 public class xmlReadTest extends AndroidTestCase {
+
+	private final static String testPath = Environment
+			.getExternalStorageDirectory().getPath() + "/readiumtest/";
+
 	public void testReadXml() throws Exception {
 		XmlReader read = new XmlReader();
-		Assert.assertEquals(14, read.getValue("bookName.xml", "bookname")
-				.size());
-		Assert.assertEquals("Creative Commons - A Shared Culture.epub", read
-				.getValue("bookName.xml", "bookname").get(0));
+		List<String> list = read
+				.getValue(testPath + "bookName.xml", "epubBook/books/bookname");
+		
+		Assert.assertEquals(14, list.size());
+		Assert.assertEquals("Creative Commons - A Shared Culture.epub",
+				list.get(0));
+		
+		DownloadFile.downLoad(list.get(0));
 	}
 }
