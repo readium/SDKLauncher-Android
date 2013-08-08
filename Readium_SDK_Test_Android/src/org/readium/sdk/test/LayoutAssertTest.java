f919e9741fa1c9997dc303e9eb15da3ce0d4e7c7
chtian <chtian@anfengde.com>
2013-08-08 11:27:02 +0800

    remove unused file

--- Readium_SDK_Test_Android/src/org/readium/sdk/test/LayoutAssertTest.java
+++ Readium_SDK_Test_Android/src/org/readium/sdk/test/LayoutAssertTest.java
@@ -1,12 +1,25 @@
 package org.readium.sdk.test;
 
+import java.util.List;
+
+import org.readium.sdk.test.util.Util;
+import org.readium.sdk.test.util.XmlReader;
+
 import android.test.ActivityInstrumentationTestCase2;
 
 public class LayoutAssertTest extends
         ActivityInstrumentationTestCase2<AssertActivity> {
 
+    /**
+     * test case list
+     */
+    private List<ReadiumTestCase> tests;
+    private AssertActivity webview;
+
+    private static boolean firstDownload = false;
+
     private AssertActivity mActivity;
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
@@ -15,17 +28,24 @@ public class LayoutAssertTest extends
 
         this.setActivityInitialTouchMode(true);
         mActivity = getActivity();
+
+        if (!firstDownload) {
+            Util.download(Util.getConfig_url(), Util.getConfig_file());
+            firstDownload = true;
+            XmlReader read = new XmlReader(Util.getConfigFullName());
+            tests = read.getTests();
+        }
     }
-    
+
     public LayoutAssertTest(Class<AssertActivity> activityClass) {
         super(activityClass);
     }
-    
+
     public LayoutAssertTest() {
         super(AssertActivity.class);
     }
-    
-    public void testTestShow(){
+
+    public void testTestShow() {
         assertTrue(mActivity != null);
     }
 }
