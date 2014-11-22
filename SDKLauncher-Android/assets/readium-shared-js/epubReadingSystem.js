4e266e91e7e2b673b5c3b3bff271c477002ecacc
Daniel Weck <daniel.weck@gmail.com>
2014-11-22 19:15:49 +0000

    working Git version infoset in epubReadingSystem, with caveat: Eclipse Builders-based system doesn't allow temporary change of "asset" resource, so need clean after building app, otherwise epubReadingSystem.js is git-committed with current values instead of empty template.

---
+++ SDKLauncher-Android/assets/readium-shared-js/epubReadingSystem.js
@@ -0,0 +1,11 @@
+
+ReadiumSDK.READIUM_Android_sha = '932a67b2b90a4ad29e2f0414be7ab4f753c7bd59';
+ReadiumSDK.READIUM_Android_tag = 'Release-0.6-93-g932a67b';
+ReadiumSDK.READIUM_Android_clean = 'false';
+ReadiumSDK.READIUM_SDK_sha = 'b9e44190a7df36e92ad710a65862fcceade531fc';
+ReadiumSDK.READIUM_SDK_tag = 'Release-0.14-122-gb9e4419';
+ReadiumSDK.READIUM_SDK_clean = 'false';
+ReadiumSDK.READIUM_SHARED_JS_sha = '811d946d619a8b28556ad5a9945fc883f95d9bbc';
+ReadiumSDK.READIUM_SHARED_JS_tag = 'Release-0.10-469-g811d946';
+ReadiumSDK.READIUM_SHARED_JS_clean = 'true';
+ReadiumSDK.READIUM_dateTimeString = 'Sat Nov 22 19:12:08 GMT 2014';
