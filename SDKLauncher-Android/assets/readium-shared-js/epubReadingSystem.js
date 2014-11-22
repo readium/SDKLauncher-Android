88437b69e554baa857d37a3564120c637eb6ece4
Daniel Weck <daniel.weck@gmail.com>
2014-11-22 19:28:29 +0000

    added missing file, restored JS empty template, added git-ignore entries

--- SDKLauncher-Android/assets/readium-shared-js/epubReadingSystem.js
+++ SDKLauncher-Android/assets/readium-shared-js/epubReadingSystem.js
@@ -1,6 +1,6 @@
 
-ReadiumSDK.READIUM_Android_sha = '932a67b2b90a4ad29e2f0414be7ab4f753c7bd59';
-ReadiumSDK.READIUM_Android_tag = 'Release-0.6-93-g932a67b';
+ReadiumSDK.READIUM_Android_sha = '92e0f7a6e27cc5ad4c0b04906fa06ec3d102a3f9';
+ReadiumSDK.READIUM_Android_tag = 'Release-0.6-95-g92e0f7a';
 ReadiumSDK.READIUM_Android_clean = 'false';
 ReadiumSDK.READIUM_SDK_sha = 'b9e44190a7df36e92ad710a65862fcceade531fc';
 ReadiumSDK.READIUM_SDK_tag = 'Release-0.14-122-gb9e4419';
@@ -8,4 +8,4 @@ ReadiumSDK.READIUM_SDK_clean = 'false';
 ReadiumSDK.READIUM_SHARED_JS_sha = '811d946d619a8b28556ad5a9945fc883f95d9bbc';
 ReadiumSDK.READIUM_SHARED_JS_tag = 'Release-0.10-469-g811d946';
 ReadiumSDK.READIUM_SHARED_JS_clean = 'true';
-ReadiumSDK.READIUM_dateTimeString = 'Sat Nov 22 19:12:08 GMT 2014';
+ReadiumSDK.READIUM_dateTimeString = 'Sat Nov 22 19:20:55 GMT 2014';
