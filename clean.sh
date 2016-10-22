2fbfaa9c00f3f9b81579907df8c8f6f72f3d6b2a
danielweck <daniel.weck@gmail.com>
2016-10-22 13:39:31 +0100

    added "clean" shell script to complement Gradle's own cleanup (which does not remove intermediary build artefacts)

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -r SDKLauncher-Android/app/build
+rm -r SDKLauncher-Android/build
+
+rm -r readium-sdk/Platform/Android/epub3/build
+rm -r readium-sdk/Platform/Android/epub3/libs
+rm -r readium-sdk/Platform/Android/epub3/obj
+
+rm -r readium-lcp-client/platform/android/lcp/build
+rm -r readium-lcp-client/platform/android/lcp/libs
+rm -r readium-lcp-client/platform/android/lcp/obj
