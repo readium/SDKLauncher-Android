5f0b1815c32c17e12f385d561ca438fd94e5e7a5
danielweck <daniel.weck@gmail.com>
2016-10-26 15:36:43 +0100

    clean script with remove includes

--- clean.sh
+++ clean.sh
@@ -3,6 +3,8 @@
 rm -r SDKLauncher-Android/app/build
 rm -r SDKLauncher-Android/build
 
+rm -r readium-sdk/Platform/Android/epub3/include
+
 rm -r readium-sdk/Platform/Android/epub3/build
 rm -r readium-sdk/Platform/Android/epub3/libs
 rm -r readium-sdk/Platform/Android/epub3/obj
