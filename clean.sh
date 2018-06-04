239afe6ce327d1bc7adf4dcbb8756d9df77cb92a
danielweck <daniel.weck@gmail.com>
2018-06-04 21:40:01 +0100

    minor clean.sh script update

--- clean.sh
+++ clean.sh
@@ -5,10 +5,12 @@ rm -r SDKLauncher-Android/build
 
 rm -r readium-sdk/Platform/Android/epub3/include
 
+rm -r readium-sdk/Platform/Android/build
 rm -r readium-sdk/Platform/Android/epub3/build
-rm -r readium-sdk/Platform/Android/epub3/libs
+# rm -r readium-sdk/Platform/Android/epub3/libs
 rm -r readium-sdk/Platform/Android/epub3/obj
 
-rm -r readium-lcp-client/platform/android/lcp/build
-rm -r readium-lcp-client/platform/android/lcp/libs
-rm -r readium-lcp-client/platform/android/lcp/obj
+rm -r readium-lcp-client/platform/android/build
+rm -r readium-lcp-client/platform/android/lib/build
+# rm -r readium-lcp-client/platform/android/lib/libs
+rm -r readium-lcp-client/platform/android/lib/obj
